549f3b3a89ae32547552eb04e53e30412f204cd0
Derrick <derrickip34@gmail.com>
2019-11-30 17:07:56 +0300

    Reject invalid date

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,15 @@ function submition(){
     var dayOfWeek =(((century/4)-2*century-1)+((5*year/4))+((26*(month+1)/10))+day)%7;
     var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
     var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Afa"]
+
+    if (document.getElementById("gender").checked) {
+        var gender = "male"
+    } else {
+        gender = "female"
+    }
+    if (month < 1 || month > 12 || month == 2 && day > 29) {
+        alert("Enter valid month")
+    } else if(day < 1 || day > 31) {
+        alert("Enter valid date")
+    }
 }
